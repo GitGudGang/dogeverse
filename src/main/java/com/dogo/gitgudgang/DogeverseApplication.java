213a066b37ff9436e999d4d3c5bf07aca083e572
SirMeows <sirmeowsh@gmail.com>
2024-09-26 14:35:07 +0200

    add basic project structure elements

---
+++ src/main/java/com/dogo/gitgudgang/DogeverseApplication.java
@@ -0,0 +1,14 @@
+package com.dogo.gitgudgang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogeverseApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogeverseApplication.class, args);
+    }
+}
