22cbabb87194186fdcc3107413b3c1d6d6fef8f2
SirMeows <sirmeowsh@gmail.com>
2024-11-28 17:25:08 +0100

    change all classes to use uuid

--- src/main/java/com/gitgudgang/dogeverse/domain/Character.java
+++ src/main/java/com/gitgudgang/dogeverse/domain/Character.java
@@ -6,12 +6,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Set;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class Character {
+    private UUID id;
     private String name;
     private Set<Stat> stats;
 }
--- src/main/java/com/gitgudgang/dogeverse/entity/CharacterEntity.java
+++ src/main/java/com/gitgudgang/dogeverse/entity/CharacterEntity.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -18,7 +19,7 @@ import java.util.Set;
 public abstract class CharacterEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     private String name;
 
